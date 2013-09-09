c6344d560bc1dcf044fb73b8f920c6b902747053
Callum Barker <callum.scott.barker@gmail.com>
2013-09-09 21:54:45 +0800

    login, logout

--- sinatra/run.rb
+++ sinatra/run.rb
@@ -1,6 +1,42 @@
-#!/usr/bin/env ruby
 require 'sinatra'
 
+enable :sessions
+
+db_array = {}
+
 get '/' do
-	'this is a simple app' 
+	erb :index
+end
+
+post '/login' do
+	# simulate database results
+	db_array[params[:user][:name]] = {
+		:username => 'test',
+		:password => 'test'
+	}
+
+	db_row = db_array[params[:user][:name]]
+
+	# validate form
+    if db_row[:username] == params[:user][:name] && db_row[:password] == params[:user][:password]
+		# form is valid, assign a session
+  		session[:username] = params[:user][:name]
+  		redirect '/todos'
+    else
+    	# form is invalid, redirect to login page
+		redirect '/'
+    end
+end
+
+get '/logout' do
+	session[:username] = nil
+	redirect "/"
+end
+
+get '/todos' do
+	 if session[:username].nil?
+	 	redirect '/'
+	 else
+	 	erb :todos
+	 end
 end
